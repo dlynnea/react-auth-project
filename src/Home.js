1ed3d0edfa7087bac57e18c42efb9c5d8ee462a2
Dani Matton <danielle@Danis-MacBook-Pro.local>
2020-02-08 17:41:15 -0700

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+    
+    render() { 
+        return (  
+            <header>
+                <h1>Home</h1>
+            </header>
+        );
+    }
+}
+ 
+export default Home;
